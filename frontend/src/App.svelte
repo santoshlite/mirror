87dd2ea69a77ad755d842c47cd4ef5a7f5dd5b30
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-11-18 00:40:16 -0500

    webcam added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
   import { Router, Route, Link } from 'svelte-routing';
+  import Home from './routes/Home.svelte'; 
   import Tryon from './routes/Tryon.svelte';
 </script>
 
-<main>
-  <Router>
-    <div>
-      <Link to="/tryon" class="nav-button">Go to Tryon</Link>
-    </div>
 
+<div class="top-bar">
+  <h1>Mirror</h1>
+</div>
+
+<main>
+
+  <Router>
+    <Route path="/" component={Home} />
     <Route path="/tryon" component={Tryon} />
   </Router>
-</main>
 
-<style>
-  .nav-button {
-    padding: 10px;
-    background-color: #646cff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    text-decoration: none;
-    margin: 10px 0;
-  }
-</style>
+</main>
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Router, Route, Link } from 'svelte-routing';
+</script>
+  
+  <h1>Homepage</h1>
+
+  <div>
+    <Link to="/tryon" class="nav-button">Try-on</Link>
+  </div>
+  
