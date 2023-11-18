89548d40f496e89862724b47d63409f1867c8d7b
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-11-18 03:33:26 -0500

    basic cart + countdown

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,12 +2,21 @@
   import { Router, Route, Link } from 'svelte-routing';
   import Home from './routes/Home.svelte'; 
   import Tryon from './routes/Tryon.svelte';
+  import logo from './assets/logo.png'; 
 </script>
 
 
 <div class="top-bar">
-  <h1>Mirror</h1>
+  <div class="spacer"></div> 
+  <a href="/" class="logo-container">
+      <img src={logo} alt="Mirror Logo" class="logo"/>
+  </a>
+  <div class="spacer"></div> 
+  <div class="icon-container">
+    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="text-xl" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M6.50488 2H17.5049C17.8196 2 18.116 2.14819 18.3049 2.4L21.0049 6V21C21.0049 21.5523 20.5572 22 20.0049 22H4.00488C3.4526 22 3.00488 21.5523 3.00488 21V6L5.70488 2.4C5.89374 2.14819 6.19013 2 6.50488 2ZM19.0049 8H5.00488V20H19.0049V8ZM18.5049 6L17.0049 4H7.00488L5.50488 6H18.5049ZM9.00488 10V12C9.00488 13.6569 10.348 15 12.0049 15C13.6617 15 15.0049 13.6569 15.0049 12V10H17.0049V12C17.0049 14.7614 14.7663 17 12.0049 17C9.24346 17 7.00488 14.7614 7.00488 12V10H9.00488Z"></path></svg>
+  </div>
 </div>
+
 
 <main>
 
--- frontend/src/routes/Tryon.svelte
+++ frontend/src/routes/Tryon.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+
+    const poses = writable([]); 
+
+
     let videoElement;
+    let canvasElement;
+    let timer = 5;
+    let countdown;
   
     function startWebcam() {
       if (navigator.mediaDevices.getUserMedia) {
@@ -13,35 +22,93 @@
       }
     }
   
-    import { onMount } from 'svelte';
+    function startCountdown() {
+      timer = 5;
+      countdown = setInterval(() => {
+        timer -= 1;
+        if (timer <= 0) {
+          clearInterval(countdown);
+          takePicture();
+        }
+      }, 1000);
+    }
+  
+    function takePicture() {
+    canvasElement.width = videoElement.videoWidth;
+    canvasElement.height = videoElement.videoHeight;
+    canvasElement.getContext('2d').drawImage(videoElement, 0, 0);
+    const imageDataURL = canvasElement.toDataURL('image/png');
+    poses.update(currentImages => [...currentImages, imageDataURL]); 
+  }
+
     onMount(() => {
       startWebcam();
+      startCountdown();
     });
   </script>
+  
+  <style>
+    .webcam-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 70px);
+    width: 50%;
+    position: absolute;
+    left: 0;
+    top: 70px; 
+    overflow: hidden;
+    }
+  
+    .webcam-video {
+      height: 100%;
+      width: 100%;
+      transform: scaleX(-1);
+      object-fit: cover;
+    }
+  
+    .timer {
+      position: absolute;
+      font-size: 3rem;
+      color: white;
+      z-index: 10;
+    }
 
-<style>
+    .image-gallery {
+    position: absolute;
+    top: 70px; 
+    right: 0;
+    width: 50%; 
+    height: calc(100vh - 70px);
+    overflow-y: auto; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
 
-.webcam-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 50%;
-  position: absolute;
-  left: 0;
-  top: 0;
-}
-
-.webcam-video {
-  height: 100%;
-  width: 100%;
-  transform: scaleX(-1);
-  object-fit: cover; 
-}
+  .captured-image {
+    max-width: 100%;
+    margin-bottom: 1rem; /* Space between images */
+    box-shadow: 0 4px 6px rgba(0,0,0,0.1); /* Optional: adds some depth to images */
+  }
 
   </style>
   
   <div class="webcam-container">
+
+    {#if timer > 0}
+      <div class="timer">{timer}</div>
+    {/if}
+
     <video bind:this={videoElement} autoplay class="webcam-video"></video>
+    <canvas bind:this={canvasElement} style="display: none;"></canvas>
   </div>
   
+  <div class="image-gallery">
+    {#each $poses as image (image)}
+      <img src={image} class="captured-image" alt="Captured snapshot">
+    {/each}
+    <button on:click={startCountdown}>Take pic</button>
+  </div>
